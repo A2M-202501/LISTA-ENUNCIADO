b2d4c3b8278e86395e29ffa6cb507a7a38d5e9c2
Lucas Pandolfo <lucas@gamma.lucas.omen>
2025-04-09 20:14:52 -0300

    Saco las tildes

--- src/cola.h
+++ src/cola.h
@@ -1,6 +1,7 @@
 #ifndef COLA_H_
 #define COLA_H_
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct cola cola_t;
 
@@ -16,39 +17,39 @@ cola_t *cola_crear();
  *
  * Devuelve true si pudo encolar o false si no pudo.
  */
-bool cola_encolar(cola_t *, void *elemento);
+bool cola_encolar(cola_t *cola, void *elemento);
 
 /**
  * Desencola un elemento de la cola y lo devuelve.
  *
  * Devuelve NULL si no hay elementos en la cola.
  */
-void *cola_desencolar(cola_t *);
+void *cola_desencolar(cola_t *cola);
 
 /**
  * Devuelve true si la cola está vacía.
  *
  * Devuelve false si no lo está.
  */
-bool cola_vacía(cola_t *);
+bool cola_vacia(cola_t *cola);
 
 /**
  * Devuelve la cantidad de elementos presentes en la cola.
  *
  * Devuelve 0 si no existe la cola.
  */
-size_t cola_tamanio(cola_t *);
+size_t cola_tamanio(cola_t *cola);
 
 /**
  * Devuelve el elemento en el frente de la cola sin desencolarlo.
  *
  * Devuelve NULL si no hay elementos en la cola.
  */
-void *cola_frente(cola_t *);
+void *cola_frente(cola_t *cola);
 
 /**
  * Destruye la cola.
  */
-void cola_destruir(cola_t *);
+void cola_destruir(cola_t *cola);
 
 #endif // COLA_H_
